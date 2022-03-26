288e4edd99ee33fe3187d2f0890fa8ae9d10c4c1
EBRIMA SONKO <82295266+ibra188@users.noreply.github.com>
2022-03-26 04:26:00 +0100

    onnect g rafici page into info page

--- src/grafici.jsx
+++ src/grafici.jsx
@@ -1,10 +1,28 @@
 import React, { Component }  from 'react';
+import { Outlet, Link } from 'react-router-dom';
+import logo from './logo.jpg';
 import './App.css';
 
 export default function grafici(){
     return(
-        <main className="graficiwrapper">
-            <h2>Grafici Page</h2>
-        </main>
+    <>
+        <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <ul className='Nav_items'>
+                        <li className='Nav_link'>
+                            <Link to="/home" className='Nav_list'>Home</Link>
+                            <Link to="/grafici" className='Nav_list'>Grafici</Link>
+                            <Link to="/info" className='Nav_list'>info</Link>
+                            <Link to="/resulti" className='Nav_list'>Resulti</Link>
+                        </li>
+                    </ul>
+                </header>
+                <main className="graficiwrapper">
+                    <h2>Grafici Page</h2>
+                 </main>
+                <Outlet />
+            </div>
+    </>
     );
-}
+};
--- src/info.jsx
+++ src/info.jsx
@@ -1,9 +1,29 @@
 import React, { Component }  from 'react';
-function info(){
-return(
-    <main className="inforwrappers">
-        <h2>Info Page</h2>
-    </main>
+import { Outlet, Link } from 'react-router-dom';
+import logo from './logo.jpg';
+import './App.css';
+
+
+
+export default info;  function info(){
+ return(
+    <> 
+       <div className='App'>
+           <header className='App-header'>
+               <img src={logo} className="App-logo" alt='log' />
+               <ul className='Nav-items'>
+                   <li className='Nav-links'>
+                       <Link to="/home" className='Nav_list'>Home</Link>
+                       <Link to="/grafici" className='Nav_list'>Grafici</Link>
+                       <Link to="/info" className='Nav_list'>Info</Link>
+                       <Link to="/resulti" className='Nav_list'>Resulti</Link>
+                   </li>
+               </ul>
+           </header>
+           <main className="inforwrappers">
+               <h2>Info Page</h2>
+            </main>
+       </div>
+    </>
 );
-}
-export default info;
+};
